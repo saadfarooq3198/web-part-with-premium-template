d2cf9400d5965b85a3922f1798ba947a89cbd02b
Saad Farooq <55571258+saadfarooq3198@users.noreply.github.com>
2022-02-01 11:56:54 +0500

    Update services.php

--- config/services.php
+++ config/services.php
@@ -30,23 +30,23 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
     'facebook' => [
-        'client_id' => '510752117046059',
-        'client_secret' => '6e82fc4028dc2b3660adde286ce9181e',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect' => 'http://127.0.0.1:8000/auth/facebook/callback',
     ],
      'github' => [
-        'client_id' => '2f5695d686dc9aa79ff2',
-        'client_secret' => '2cd98350d8141ea6d8435e4f17b6aed989885286',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect' => 'http://127.0.0.1:8000/auth/github/callback',
     ],
     'google' => [
-        'client_id' => '513766243418-5l3st9v3tgbls9qfu00neps9spdargqe.apps.googleusercontent.com',
-        'client_secret' => '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',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect' => 'http://127.0.0.1:8000/auth/google/callback',
       ], 
       'linkedin' => [
-        'client_id' => '777d4jcq3cjy4f',
-        'client_secret' => 'mmNngLGv9N3O3meS',
+        'client_id' => '',
+        'client_secret' => '',
         'redirect' => 'http://127.0.0.1:8000/auth/linkedin/callback',
     ],
 ];
